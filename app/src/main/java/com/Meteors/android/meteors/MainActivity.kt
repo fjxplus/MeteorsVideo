bd5e0030f79f9f8378c3aced298f45cf0c2e7a5a
xingxiaofan <2401540938@qq.com>
2022-03-26 22:44:34 +0800

    增加MediaPlayerPool

--- app/src/main/java/com/Meteors/android/meteors/MainActivity.kt
+++ app/src/main/java/com/Meteors/android/meteors/MainActivity.kt
@@ -22,22 +22,30 @@ import com.Meteors.android.meteors.databinding.VideoItemBinding
 import java.lang.Exception
 
 private const val TAG = "VideoAdapter"
+private const val TAG1 = "Test"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var mediaPlayerPool: MediaPlayerPool
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
+
         val pagerSnapHelper = MyPagerSnapHelper()
-        var videoAdapter = VideoAdapter(listOf("VID_1.mp4", "VID_2.mp4", "VID_3.mp4", "VID_4.mp4", "VID_5.mp4", "VID_6.mp4"))
+        val videoUri =
+            listOf("VID_1.mp4", "VID_2.mp4", "VID_3.mp4", "VID_4.mp4", "VID_5.mp4", "VID_6.mp4")
+
+        mediaPlayerPool = MediaPlayerPool(this, videoUri)
+
+        var videoAdapter = VideoAdapter(videoUri)
         viewBinding.recyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = videoAdapter
             pagerSnapHelper.attachToRecyclerView(this)
-            addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
                 override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                     super.onScrollStateChanged(recyclerView, newState)
                     videoAdapter.onScrollStateChanged(newState)
@@ -57,99 +65,83 @@ class MainActivity : AppCompatActivity() {
     }
 
     private inner class VideoHolder(val itemBinding: VideoItemBinding) :
-        RecyclerView.ViewHolder(itemBinding.root), SurfaceHolder.Callback, View.OnClickListener {
+        RecyclerView.ViewHolder(itemBinding.root), View.OnClickListener, SurfaceHolder.Callback {
 
         private lateinit var mediaPlayer: MediaPlayer
-        private lateinit var videoUri: String
+        private var curPosition = 0
         private val holder: SurfaceHolder = itemBinding.video.holder
         private var isInitialized = false
 
         @RequiresApi(Build.VERSION_CODES.N)
-        fun onBind(videoUri: String) {
-            Log.d(TAG, "viewHolder onBind(), Uri is $videoUri")
-            this.videoUri = videoUri
+        fun onBind(position: Int, videoUri: String) {
+            Log.d(TAG, "viewHolder($position) onBind()")
+            this.curPosition = position
+            mediaPlayerPool.load(position)
             itemBinding.root.setOnClickListener(this)
             holder.addCallback(this)
         }
 
-        fun initMediaPlayer(){
-            mediaPlayer = MediaPlayer()
-            isInitialized = true
-            try {
-                val fd = assets.openFd(videoUri)
-                mediaPlayer.setDataSource(fd.fileDescriptor, fd.startOffset, fd.length)
 
-                mediaPlayer.prepareAsync()
-            }catch (e: Exception){
-                Toast.makeText(this@MainActivity, "视频加载失败", Toast.LENGTH_SHORT).show()
-                e.printStackTrace()
-            }
+        fun videoStart() {
 
         }
 
-        fun videoStart(){
-            if(!isInitialized){
-                initMediaPlayer()
-            }else{
-                mediaPlayer.start()
-            }
-            itemBinding.btnPause.visibility = View.INVISIBLE
-        }
+        fun videoPause() {
 
-        fun videoPause(){
-            if (mediaPlayer.isPlaying){
-                mediaPlayer.pause()
-                itemBinding.btnPause.visibility = View.VISIBLE
-            }
-        }
-
-        fun releaseMediaPlayer() {
-            Log.d(TAG, "mediaPlayer release()")
-            if(isInitialized){
-                mediaPlayer.release()
-                isInitialized = false
-            }
-        }
-
-        override fun surfaceCreated(holder: SurfaceHolder) {
-            mediaPlayer.setDisplay(holder)
-            mediaPlayer.setOnPreparedListener {
-                Log.d(TAG, "surfaceCreated and mediaPlayer prepared()")
-                videoStart()
-            }
-            mediaPlayer.setOnCompletionListener {
-                itemBinding.btnPause.visibility = View.VISIBLE
-            }
-        }
-
-        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        }
-
-        override fun surfaceDestroyed(holder: SurfaceHolder) {
         }
 
         override fun onClick(v: View?) {
-            when(v?.id){
+            when (v?.id) {
 
-                R.id.btn_praise ->{
+                R.id.btn_praise -> {
 
                 }
-                R.id.btn_comment ->{
+                R.id.btn_comment -> {
 
                 }
                 else -> {
-                    if(mediaPlayer.isPlaying){
-                        videoPause()
-                    }else{
-                        videoStart()
+                    if (mediaPlayerPool.isPaused()) {
+                        startVideo()
+                        itemBinding.btnPause.visibility = View.INVISIBLE
+                    } else {
+                        mediaPlayerPool.pauseVideo()
+                        itemBinding.btnPause.visibility = View.VISIBLE
                     }
                 }
             }
         }
+
+        fun startVideo() {
+            mediaPlayerPool.startVideo(curPosition, holder, isInitialized)
+            Log.d(TAG1, "Holder-${curPosition} startVideo()")
+        }
+
+        override fun surfaceCreated(holder: SurfaceHolder) {
+            Log.d(TAG1, "surfaceCreated")
+            mediaPlayerPool.surfaceCreatedFinished(curPosition, holder)
+            isInitialized = true
+        }
+
+        override fun surfaceChanged(
+            holder: SurfaceHolder,
+            format: Int,
+            width: Int,
+            height: Int
+        ) {
+        }
+
+        override fun surfaceDestroyed(holder: SurfaceHolder) {}
     }
 
     private inner class VideoAdapter(val videoList: List<String>) :
-        RecyclerView.Adapter<VideoHolder>()  {
+        RecyclerView.Adapter<VideoHolder>() {
+
+        private var curHolder = HashSet<VideoHolder>()      //用于保存当前屏幕上的VideoHolder
+
+        private var scrollState = 0     //用于判断滚动状态
+
+        private var isFirstBind = true     //判断是否为第一次执行onBind()
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoHolder {
             val itemBinding = VideoItemBinding.inflate(layoutInflater, parent, false)
             return VideoHolder(itemBinding)
@@ -158,7 +150,11 @@ class MainActivity : AppCompatActivity() {
         @RequiresApi(Build.VERSION_CODES.N)
         override fun onBindViewHolder(holder: VideoHolder, position: Int) {
             val videoUri = videoList[position]
-            holder.onBind(videoUri)
+            holder.onBind(position, videoUri)
+            if (isFirstBind) {
+                holder.startVideo()
+                isFirstBind = false
+            }
         }
 
         override fun getItemCount(): Int {
@@ -167,34 +163,46 @@ class MainActivity : AppCompatActivity() {
 
         override fun onViewRecycled(holder: VideoHolder) {
             super.onViewRecycled(holder)
-            holder.releaseMediaPlayer()
-            Log.d(TAG, "holder onViewRecycled()")
         }
 
+        /**
+         * 新的VideoHolder出新在屏幕上
+         */
         override fun onViewAttachedToWindow(holder: VideoHolder) {
             super.onViewAttachedToWindow(holder)
+            curHolder.add(holder)
             holder.videoStart()
-            Log.d(TAG, "holder attachedToWindow()")
         }
 
+        /**
+         * VideoHolder滑出屏幕
+         */
         override fun onViewDetachedFromWindow(holder: VideoHolder) {
             super.onViewDetachedFromWindow(holder)
-            Log.d(TAG, "holder detachedToWindow()")
+            curHolder.remove(holder)
             holder.videoPause()
         }
 
-        fun onScrollStateChanged(newState: Int){
-            when(newState){
+        /**
+         * 对RecyclerView的滑动事件进行监听
+         * 当滑动之后页面不变的情况下，SCROLL_STATE_IDLE会进行两次回调，中间发生一次SCROLL_STATE_SETTLING
+         * scrollState用来判断滚动事件的结束，我们只要最后一次SCROLL_STATE_IDLE
+         */
+        fun onScrollStateChanged(newState: Int) {
+            when (newState) {
                 RecyclerView.SCROLL_STATE_IDLE -> {
-                    Log.d(TAG, "滑动停止")
+                    if (scrollState == 1) {
+                        curHolder.first().startVideo()
+                        scrollState = 0
+                    }
                 }
-
+                RecyclerView.SCROLL_STATE_SETTLING -> {
+                    scrollState++
+                }
             }
         }
 
     }
 
-    private class MyPagerSnapHelper : PagerSnapHelper() {
-
-    }
+    private class MyPagerSnapHelper : PagerSnapHelper() {}
 }
--- app/src/main/java/com/Meteors/android/meteors/MediaPlayerPool.kt
+++ app/src/main/java/com/Meteors/android/meteors/MediaPlayerPool.kt
@@ -2,62 +2,105 @@ package com.Meteors.android.meteors
 
 import android.content.Context
 import android.media.MediaPlayer
+import android.util.Log
 import android.util.LruCache
 import android.view.SurfaceHolder
 import android.widget.Toast
 import java.lang.Exception
 
+private const val TAG = "VideoAdapter"
+private const val TAG1 = "Test"
 class MediaPlayerPool(private val context: Context, private val list: List<String>) {
     private val mLruCache = LruCache<Int, MediaPlayer>(5)
     private val preCacheCount = 2
+    private var player: MediaPlayer? = null
+    private var curPosition = -1
+    private val hashmap = HashMap<Int, Boolean>()
 
-    fun bind(position: Int, surfaceHolder: SurfaceHolder){
-        var curPlayer = mLruCache.get(position)
-        if(curPlayer == null){
-            curPlayer = MediaPlayer()
-            try {
-                val fd = context.assets.openFd(list[position])
-                curPlayer.setDataSource(fd.fileDescriptor)
-                curPlayer.prepareAsync()
-            }catch (e: Exception){
-                Toast.makeText(context, "视频加载失败", Toast.LENGTH_SHORT).show()
-                e.printStackTrace()
-            }
-            mLruCache.put(position, curPlayer)
-            //创建缓存
-            for (i in 1..preCacheCount){
-                if(position + i < list.size){
-                    val player = MediaPlayer()
-                    try {
-                        val fd = context.assets.openFd(list[position])
-                        player.setDataSource(fd.fileDescriptor)
-                        player.prepareAsync()
-                    }catch (e: Exception){
-                        Toast.makeText(context, "视频预加载失败", Toast.LENGTH_SHORT).show()
-                        e.printStackTrace()
-                    }
-                    mLruCache.put(position, player)
-                }
+    fun load(position: Int) {
+        val player = mediaPlayerInstance(position)
+        //创建缓存
+        for (i in 1..preCacheCount) {
+            if (position + i < list.size) {
+                mediaPlayerInstance(position + i)
             }
         }
     }
 
-    fun mediaPlayerConstructor(position: Int): MediaPlayer{
-        var player: MediaPlayer = mLruCache.get(position)
-        player.duration
-        if(player == null){
-            val player = MediaPlayer()
+    fun startVideo(position: Int, surfaceHolder: SurfaceHolder, holderInitialized: Boolean){
+        pauseVideo()
+        Log.d(TAG1, "curPosition is $curPosition, position is $position, $holderInitialized")
+
+        curPosition = position
+
+        if(holderInitialized){
+            player = mediaPlayerInstance(position)
+            player!!.setDisplay(surfaceHolder)
+            //出问题
+            if(hashmap[position] == false){
+                player!!.setOnPreparedListener{
+                    Log.d(TAG1, "player prepared()")
+                    player!!.start()
+                }
+            }else{
+                player!!.start()
+            }
+
+        }
+    }
+
+    fun surfaceCreatedFinished(position: Int, surfaceHolder: SurfaceHolder){
+        Log.d(TAG1, "curPosition is $curPosition, position is $position")
+
+            startVideo(position, surfaceHolder, true)
+
+    }
+
+    fun pauseVideo(){
+        player?.pause()
+    }
+
+    fun isPaused(): Boolean{
+        player?.apply {
+            return !isPlaying
+        }
+        return true
+    }
+
+    private fun mediaPlayerInstance(position: Int): MediaPlayer {
+        var player: MediaPlayer? = mLruCache.get(position)
+
+        if (player == null) {
+            player = MediaPlayer()
             try {
                 val fd = context.assets.openFd(list[position])
-                player.setDataSource(fd.fileDescriptor)
+                player.setDataSource(fd.fileDescriptor, fd.startOffset, fd.length)
                 player.prepareAsync()
-            }catch (e: Exception){
+                player.setOnPreparedListener{
+                    Log.d(TAG1, "player prepared()")
+                    hashmap[position] = true
+                }
+            } catch (e: Exception) {
                 Toast.makeText(context, "视频-${list[position]} 加载失败", Toast.LENGTH_SHORT).show()
                 e.printStackTrace()
             }
+
             mLruCache.put(position, player)
         }
 
         return player
     }
+
+    /*
+    private fun releasePlayer(){
+        Log.d(TAG, "mediaPlayer release()")
+        if(isInitialized){
+            mediaPlayer.release()
+            isInitialized = false
+        }
+    }
+
+     */
+
+
 }
